ee71378e28105f0e636632ea7daf756ced9924d3
David Tilšer <davidtilser@gmail.com>
2022-02-16 13:02:22 +0100

    VEX-53 Added schema annotations for swagger

--- src/main/java/com/cleevio/vexl/module/user/dto/request/SignatureRequest.java
+++ src/main/java/com/cleevio/vexl/module/user/dto/request/SignatureRequest.java
@@ -1,13 +1,21 @@
 package com.cleevio.vexl.module.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
 public class SignatureRequest {
 
+    @Schema(required = true)
+    @NotBlank
     String publicKey;
+
+    @Schema(required = true)
+    @NotBlank
     String phoneNumber;
 
 }
