c4a2415221e69fa4e825600e33a122c9783cdc7e
David Tilšer <davidtilser@gmail.com>
2022-02-16 11:28:43 +0100

    Prepared basic layer for tests

--- src/test/java/com/cleevio/vexl/ApplicationTests.java
+++ src/test/java/com/cleevio/vexl/ApplicationTests.java
@@ -1,10 +1,11 @@
 package com.cleevio.vexl;
 
+import com.cleevio.vexl.common.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ApplicationIT {
+@IntegrationTest
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ src/test/java/com/cleevio/vexl/common/BaseControllerTest.java
@@ -0,0 +1,56 @@
+package com.cleevio.vexl.common;
+
+import com.cleevio.vexl.module.user.entity.User;
+import com.cleevio.vexl.module.user.entity.UserVerification;
+import com.cleevio.vexl.module.user.service.SignatureService;
+import com.cleevio.vexl.module.user.service.UserService;
+import com.cleevio.vexl.module.user.service.UserVerificationService;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+
+public class BaseControllerTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @MockBean
+    protected UserService userService;
+
+    @MockBean
+    protected SignatureService signatureService;
+
+    @MockBean
+    protected UserVerificationService userVerificationService;
+
+    @Mock
+    protected User user;
+
+    @Mock
+    protected UserVerification userVerification;
+
+    @BeforeEach
+    @SneakyThrows
+    public void setup() {
+        Mockito.when(user.getId()).thenReturn(1L);
+        Mockito.when(user.getUsername()).thenReturn("Cermak");
+        Mockito.when(user.getPublicKey()).thenReturn("bd1f51asa1sd51sad55a91ds9as");
+
+        Mockito.when(userVerification.getId()).thenReturn(1L);
+        Mockito.when(userVerification.getVerificationCode()).thenReturn("456");
+        Mockito.when(userVerification.getExpirationAt()).thenReturn(Instant.now());
+
+        Mockito.when(userService.existsUserByUsername(any())).thenReturn(false);
+        Mockito.when(userService.findByPublicKey(any())).thenReturn(Optional.of(user));
+
+    }
+}
