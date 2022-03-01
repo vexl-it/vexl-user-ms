cf52371a5809c8cc6863415496363d7b5c1a6541
David Tilšer <davidtilser@gmail.com>
2022-03-01 14:19:39 +0100

    Added handle error of signature in authentication, adjust security filter, changed api URLs to nouns

--- src/main/java/com/cleevio/vexl/common/security/filter/SecurityFilter.java
+++ src/main/java/com/cleevio/vexl/common/security/filter/SecurityFilter.java
@@ -1,19 +1,24 @@
 package com.cleevio.vexl.common.security.filter;
 
+import com.cleevio.vexl.common.dto.ErrorResponse;
 import com.cleevio.vexl.common.security.AuthenticationHolder;
 import com.cleevio.vexl.module.user.enums.AlgorithmEnum;
 import com.cleevio.vexl.module.user.exception.DigitalSignatureException;
 import com.cleevio.vexl.module.user.service.SignatureService;
 import com.cleevio.vexl.module.user.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collections;
 
 public class SecurityFilter extends OncePerRequestFilter {
 
@@ -28,12 +33,13 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String requestURI = request.getRequestURI();
 
         String publicKey = request.getHeader("public-key");
         String phoneHash = request.getHeader("phone-hash");
         String signature = request.getHeader("signature");
 
-        if (signature == null || publicKey == null || phoneHash == null) {
+        if (signature == null || publicKey == null || phoneHash == null || !requestURI.contains("/api/v1")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -56,9 +62,22 @@ public class SecurityFilter extends OncePerRequestFilter {
             }
         } catch (DigitalSignatureException e) {
             SecurityContextHolder.clearContext();
-            //todo handleError
+            handleError(response, "Signature verification failed: " + e.getMessage(), Integer.parseInt(e.getErrorCode()));
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    protected void handleError(ServletResponse response, String s, int code) throws IOException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setStatus(code);
+
+        ErrorResponse error = new ErrorResponse(Collections.singleton(s), "0", Collections.emptyMap());
+        OutputStream out = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(out, error);
+        out.flush();
+
+        throw new RuntimeException();
     }
 }
