0c1925ffa25701b1029d56eff52854ba4394b254
David Tilšer <davidtilser@gmail.com>
2022-03-18 13:23:35 +0100

    Added trimming for String values in requests

--- src/main/java/com/cleevio/vexl/module/user/dto/request/PhoneConfirmRequest.java
+++ src/main/java/com/cleevio/vexl/module/user/dto/request/PhoneConfirmRequest.java
@@ -1,5 +1,7 @@
 package com.cleevio.vexl.module.user.dto.request;
 
+import com.cleevio.vexl.module.user.serializer.TrimStringDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -12,5 +14,6 @@ public class PhoneConfirmRequest {
     @Pattern(regexp = "^\\+(?:[0-9] ?){6,14}[0-9]$")
     @NotBlank
     @Schema(required = true, description = "Phone must be valid according industry-standard notation pattern specified by ITU-T E.123")
+    @JsonDeserialize(using = TrimStringDeserializer.class)
     private String phoneNumber;
 }
--- src/main/java/com/cleevio/vexl/module/user/dto/request/UserCreateRequest.java
+++ src/main/java/com/cleevio/vexl/module/user/dto/request/UserCreateRequest.java
@@ -1,5 +1,7 @@
 package com.cleevio.vexl.module.user.dto.request;
 
+import com.cleevio.vexl.module.user.serializer.TrimStringDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -10,9 +12,11 @@ public class UserCreateRequest {
 
     @NotBlank
     @Schema(required = true, description = "Username in String format")
+    @JsonDeserialize(using = TrimStringDeserializer.class)
     private final String username;
 
     @Schema(description = "Base64 encoded file data including header. i.e.: data:image/png;base64,iVBORw0KGgo")
+    @JsonDeserialize(using = TrimStringDeserializer.class)
     private final String avatar;
 
     public static UserCreateRequest of(String username, String avatar) {
