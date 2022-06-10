9b6cdec4dfa0d067120207ed7c3ed23c93af3359
David Tilšer <davidtilser@gmail.com>
2022-06-10 20:53:45 +0200

    VEX-234 History of Bitcoin price

--- src/main/java/com/cleevio/vexl/module/cryptocurrency/service/CryptocurrencyService.java
+++ src/main/java/com/cleevio/vexl/module/cryptocurrency/service/CryptocurrencyService.java
@@ -1,6 +1,7 @@
 package com.cleevio.vexl.module.cryptocurrency.service;
 
 import com.cleevio.vexl.module.cryptocurrency.dto.response.CoingeckoResponse;
+import com.cleevio.vexl.module.cryptocurrency.dto.response.MarketChartResponse;
 import com.cleevio.vexl.module.cryptocurrency.exception.CoinException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +9,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Objects;
 
 /**
@@ -24,6 +27,9 @@ public class CryptocurrencyService {
 
     @Value("${coingecko.url.coin}")
     private String coingeckoUrlCoinApi;
+
+    @Value("${coingecko.url.market-chart}")
+    private String coingeckoUrlMarketChartApi;
 
     /**
      * Retrieve coin price from Coingecko. As parameter must be supported name of cryptocurrency.
@@ -42,5 +48,25 @@ public class CryptocurrencyService {
             throw new CoinException();
         }
 
+    }
+
+    public MarketChartResponse retrieveMarketChart(String from, String to) throws CoinException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        URI targetUrl = UriComponentsBuilder.fromUriString(this.coingeckoUrl)
+                .path(this.coingeckoUrlMarketChartApi)
+                .queryParam("vs_currency", "USD")
+                .queryParam("from", from)
+                .queryParam("to", to)
+                .build()
+                .encode()
+                .toUri();
+
+        try {
+            return Objects.requireNonNull(restTemplate.getForObject(targetUrl, MarketChartResponse.class));
+        } catch (Exception e) {
+            log.error("Error occurred during retrieval of data from Coingecko", e);
+            throw new CoinException();
+        }
     }
 }
