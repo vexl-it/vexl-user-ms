f2ffd3e2e89e3739655a6ffc6799cad6e5c09fdc
David Tilšer <davidtilser@gmail.com>
2022-08-22 20:18:52 +0200

    VEX-815 File path manipulation (code, API)

--- src/main/java/com/cleevio/vexl/module/file/dto/request/ImageRequest.java
+++ src/main/java/com/cleevio/vexl/module/file/dto/request/ImageRequest.java
@@ -1,6 +1,6 @@
 package com.cleevio.vexl.module.file.dto.request;
 
-import com.cleevio.vexl.common.annotation.OnlyPng;
+import com.cleevio.vexl.common.annotation.CheckValidImageExtension;
 import com.cleevio.vexl.module.user.annotation.ValidAvatar;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,8 +13,8 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class ImageRequest {
 
-    @OnlyPng
     @NotBlank
+    @CheckValidImageExtension
     private String extension;
 
     @NotBlank
