0a3dc7b0ef641dcf0a73f94b46d5a5a5ad657e8f
David Tilšer <davidtilser@gmail.com>
2022-08-29 15:48:23 +0200

    Modified verification code exception message.

--- src/main/java/com/cleevio/vexl/module/user/exception/UserErrorType.java
+++ src/main/java/com/cleevio/vexl/module/user/exception/UserErrorType.java
@@ -10,7 +10,7 @@ public enum UserErrorType implements ErrorType {
 
     USER_DUPLICATE("101", "User already exists."),
 	USER_NOT_FOUND("103", "User not found."),
-	VERIFICATION_EXPIRED("104", "Verification code expired."),
+	VERIFICATION_EXPIRED("104", "Verification code expired or you entered the wrong code."),
 	SIGNATURE_ERROR("105", "Error occurred during creating signature."),
 	CHALLENGE_ERROR("106", "Error occurred during generating challenge."),
 	INVALID_PK_HASH("108", "Server could not create message for signature. Public key or hash is invalid."),
