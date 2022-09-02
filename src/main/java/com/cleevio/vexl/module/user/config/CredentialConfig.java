3b98e821a2fbf19a952a4917bf1caff8dc2c7e59
David Tilšer <davidtilser@gmail.com>
2022-09-02 10:04:14 +0200

    VEX-960 Adding credentials to stage as well

--- src/main/java/com/cleevio/vexl/module/user/config/CredentialConfig.java
+++ src/main/java/com/cleevio/vexl/module/user/config/CredentialConfig.java
@@ -6,6 +6,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Validated
@@ -17,7 +18,10 @@ public record CredentialConfig(
 
         @Nullable
         @NullOrNotBlank
-        String code
+        String code,
+
+        @NotNull
+        Boolean active
 
 ) {
 }
